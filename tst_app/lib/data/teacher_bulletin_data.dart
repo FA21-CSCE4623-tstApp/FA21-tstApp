4f5b2aa1d1fa44c6dbb3d08385bd27f7049b766b
Tristan Newsome <89608266+Tnewsome28@users.noreply.github.com>
2021-12-01 18:23:31 -0600

    Update teacher_bulletin_data.dart
    added functionality to save blog in database + other functions to help clear/set/get fields

--- tst_app/lib/data/teacher_bulletin_data.dart
+++ tst_app/lib/data/teacher_bulletin_data.dart
@@ -4,10 +4,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:http/http.dart' as http;
 
 class TBData extends ChangeNotifier {
+  final CollectionReference blogPost =
+  FirebaseFirestore.instance.collection('Blog_Posts');
+  
   Future _getBlogPosts() async {
     // get document from firebase
     CollectionReference collectionReference =
-        FirebaseFirestore.instance.collection('Posts');
+    FirebaseFirestore.instance.collection('Posts');
+
+   
 
     //return default value if error
 
@@ -33,10 +38,95 @@ class TBData extends ChangeNotifier {
     }
   }
 
+
+  Future createBlogPosts() async{
+    List postData = [{
+      'title': getTitle(),
+      'body': getBody(),
+      'created_time':  Timestamp.now(),
+      'tags': getTags(),
+      'author': getAuthor(),
+    }];
+    print(postData);
+    print('button clicked');
+    //change vvv to post so in one collection
+    return blogPost
+        .doc()
+        .set({
+      'title': getTitle(),
+      'body': getBody(),
+      'created_time':  Timestamp.now(),
+      'tags': getTags(),
+      'author': getAuthor(),
+    })
+        .then((value) => print("Added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
+
+  List<String> fields = ['blogTitle', 'blogAuthor', 'blogBody'];
+  void clearFields(){
+    fields.clear();
+    notifyListeners();
+  }
+
   bool choice = false;
   bool getShowScreen() {
     return choice;
   }
+
+  List<String> userTags = [];
+  List<String> getTags(){
+    return userTags;
+  }
+
+  void set setTags(String choice){
+    // userTags.add(choice);
+    isDuplicate(choice);
+    notifyListeners();
+  }
+  void clearTags(){
+    userTags.clear();
+    notifyListeners();
+  }
+
+  void clearTag(String choice){
+    userTags.remove(choice);
+    notifyListeners();
+  }
+
+  String blogTitle = "";
+  String getTitle(){
+    return blogTitle;
+  }
+
+  void setTitle(String title){
+    blogTitle = title;
+  }
+
+
+  String blogBody = "";
+  String getBody(){
+    return blogBody;
+  }
+  void setBody(String body){
+    blogBody = body;
+  }
+
+  String blogAuthor = "";
+  String getAuthor(){
+    return blogAuthor;
+  }
+
+  void setAuthor(String author){
+    blogAuthor = author;
+  }
+
+  void isDuplicate(String choice){
+    if(!userTags.contains(choice)){
+      userTags.add(choice);
+    }
+  }
+  
 
   // used to display information on post dialog that pops up
   Color postBackgroundColor = Colors.transparent; // used to show post image
@@ -60,6 +150,9 @@ class TBData extends ChangeNotifier {
   //  getters and setters
   Future get postData => _getBlogPosts();
   bool get showScreen => getShowScreen();
+  Future get blogData => createBlogPosts();
+  List<String> get postTag => getTags();
+
   void set setShowScreen(bool show) {
     choice = show;
     notifyListeners();
