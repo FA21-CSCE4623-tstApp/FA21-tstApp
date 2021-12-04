f4ad30654a09ecfe6a14b3f7f9674ad0704f9cd7
vbhalter <vbhalter@uark.edu>
2021-12-04 13:13:13 -0600

    Improved look of events section and added monthly event.

--- tst_app/lib/resources/all_events.dart
+++ tst_app/lib/resources/all_events.dart
@@ -1,8 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:tst_app/data/events_data.dart';
+import 'package:tst_app/give/monthly_event.dart';
 import 'package:tst_app/styles.dart';
 import 'package:tst_app/home/page_components.dart';
 
@@ -46,7 +49,7 @@ class AllEvents extends StatelessWidget {
                           textAlign: TextAlign.left,
                           softWrap: false,
                           style: const TextStyle(
-                              fontSize: 20, color: Colors.black)),
+                              fontSize: 20, color: defaultTextColor)),
                       Text(
                         (upcEvents[index]["date_time"] != null)
                             ? headerDate
@@ -54,7 +57,8 @@ class AllEvents extends StatelessWidget {
                                     upcEvents[index]["date_time"]))
                                 .toString()
                             : "Today",
-                        textAlign: TextAlign.left,
+                        textAlign: TextAlign.left,style: const TextStyle(
+                          color: defaultTextColor)
                       )
                     ],
                   )),
@@ -133,6 +137,7 @@ class AllEvents extends StatelessWidget {
                                                       )),
                                                 ),
                                               ),
+                                              SizedBox(height: 8),
                                               Container(
                                                   padding:
                                                       const EdgeInsets.only(
@@ -146,7 +151,7 @@ class AllEvents extends StatelessWidget {
                                                           : "Untitled Event",
                                                       style: const TextStyle(
                                                           fontSize: 22))),
-                                              const SizedBox(height: 5),
+                                              const SizedBox(height: 8),
                                               Container(
                                                   padding:
                                                       const EdgeInsets.only(
@@ -159,8 +164,9 @@ class AllEvents extends StatelessWidget {
                                                               ["event_location"]
                                                           : "Virtual Event",
                                                       style: const TextStyle(
-                                                          fontSize: 16))),
-                                              const SizedBox(height: 5),
+                                                          fontSize: 16, color:
+                                                      defaultTextColor))),
+                                              const SizedBox(height: 8),
                                               Container(
                                                   padding:
                                                       const EdgeInsets.only(
@@ -178,7 +184,8 @@ class AllEvents extends StatelessWidget {
                                                               .toString()
                                                           : "Today",
                                                       style: const TextStyle(
-                                                          fontSize: 14))),
+                                                          fontSize: 14, color:
+                                                      defaultTextColor))),
                                               const SizedBox(height: 20),
                                               Container(
                                                   padding:
@@ -242,7 +249,7 @@ class AllEvents extends StatelessWidget {
             );
           });
     } else {
-      return Text("No Upcoming Events", style: TextStyle(fontSize: 20));
+      return Text("No Upcoming Events", style: TextStyle(fontSize: 20, color:defaultTextColor));
     }
   }
 
@@ -271,7 +278,7 @@ class AllEvents extends StatelessWidget {
                           textAlign: TextAlign.left,
                           softWrap: false,
                           style: const TextStyle(
-                              fontSize: 20, color: Colors.black)),
+                              fontSize: 20, color: defaultTextColor)),
                       Text(
                         (prevEvents[index]["date_time"] != null)
                             ? headerDate
@@ -280,6 +287,7 @@ class AllEvents extends StatelessWidget {
                                 .toString()
                             : "Today",
                         textAlign: TextAlign.left,
+                        style: TextStyle(color: defaultTextColor)
                       )
                     ],
                   )),
@@ -358,6 +366,7 @@ class AllEvents extends StatelessWidget {
                                                       )),
                                                 ),
                                               ),
+                                              SizedBox(height: 8),
                                               Container(
                                                   padding:
                                                       const EdgeInsets.only(
@@ -371,7 +380,7 @@ class AllEvents extends StatelessWidget {
                                                           : "Untitled Event",
                                                       style: const TextStyle(
                                                           fontSize: 22))),
-                                              const SizedBox(height: 5),
+                                              const SizedBox(height: 8),
                                               Container(
                                                   padding:
                                                       const EdgeInsets.only(
@@ -384,8 +393,9 @@ class AllEvents extends StatelessWidget {
                                                               ["event_location"]
                                                           : "Virtual Event",
                                                       style: const TextStyle(
-                                                          fontSize: 16))),
-                                              const SizedBox(height: 5),
+                                                          fontSize: 16,
+                                                          color: defaultTextColor))),
+                                              const SizedBox(height: 8),
                                               Container(
                                                   padding:
                                                       const EdgeInsets.only(
@@ -403,7 +413,8 @@ class AllEvents extends StatelessWidget {
                                                               .toString()
                                                           : "Today",
                                                       style: const TextStyle(
-                                                          fontSize: 14))),
+                                                          fontSize: 14,
+                                                          color: defaultTextColor))),
                                               const SizedBox(height: 20),
                                               Container(
                                                   padding:
@@ -467,7 +478,7 @@ class AllEvents extends StatelessWidget {
             );
           });
     } else {
-      return Text("No Upcoming Events", style: TextStyle(fontSize: 20));
+      return Text("No Upcoming Events", style: TextStyle(fontSize: 20, color:defaultTextColor));
     }
   }
 
@@ -521,9 +532,9 @@ class AllEvents extends StatelessWidget {
                                 title: const Text(
                                   'Upcoming Events',
                                   style: TextStyle(
-                                    fontSize: 25.0,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30.0,
+                                    color: defaultTextColor,
+                                    fontWeight: FontWeight.w500,
                                   ),
                                 ),
                                 children: [
@@ -540,9 +551,9 @@ class AllEvents extends StatelessWidget {
                                 title: const Text(
                                   'Previous Events',
                                   style: TextStyle(
-                                    fontSize: 25.0,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30.0,
+                                    color: defaultTextColor,
+                                    fontWeight: FontWeight.w500,
                                   ),
                                 ),
                                 children: [
@@ -595,6 +606,93 @@ class AllEvents extends StatelessWidget {
                               ),
                             ),
                           ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 80.0,
+                                  left: 8.0,
+                                ),
+                                child: Text(
+                                  'Event of the Month',
+                                  style:
+                                  TextStyle(color: defaultTextColor, fontSize: 23.0),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8.0, bottom: 40.0),
+                                child: Container(
+                                  color: Colors.black87,
+                                  height: 270.0,
+                                  width: double.infinity,
+                                  child: Stack(
+                                      children: <Widget>[
+                                        Container(
+                                          width: double.infinity,
+                                          height: 270.0,
+                                          child: Image.asset(
+                                            'assets/images/TeachersLounge.jpg',
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        Container(
+                                          height: 270.0,
+                                          width: double.infinity,
+                                          child: BackdropFilter(
+                                            filter:
+                                            ImageFilter.blur(sigmaX: 2, sigmaY: 3),
+                                            child: Container(
+                                              color: Colors.black54,
+                                            ),
+                                          ),
+                                        ),
+                                        Positioned(
+                                          left: 12.0,
+                                          top: 8.0,
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Text(
+                                                'July 30',
+                                                style: TextStyle(
+                                                  fontSize: 40.0,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w200,
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                height: 85.0,
+                                              ),
+                                              Text(
+                                                'Let\'s Chat',
+                                                style: TextStyle(
+                                                  fontSize: 50.0,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w300,
+                                                ),
+                                              ),
+                                              Container(
+                                                width: MediaQuery.of(context).size.width,
+                                                child: Text(
+                                                  'We have partnered with local licensed counselor Makesha Parrish',
+                                                  style: TextStyle(
+                                                    fontSize: 17.0,
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.w300,
+                                                    fontStyle: FontStyle.italic,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          )
                         ],
                       ),
                     ),
